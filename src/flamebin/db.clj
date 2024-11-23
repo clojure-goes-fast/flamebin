5ced0ef3e19ee9e4606428d08d36191def2e5599
Oleksandr Yakushev <alex@bytopia.org>
2024-11-24 00:24:53 +0200

    Fix DB tests

--- src/flamebin/db.clj
+++ src/flamebin/db.clj
@@ -12,28 +12,26 @@
 
 ;;;; Preparation
 
-(def ^:private db-lock (ReentrantLock.))
-
-(defn- db-options []
-  {:dbtype "sqlite"
-   :dbname (@config :db :path)})
-
-(defn- migratus-config []
+(defn- migratus-config [connectable]
   {:store                :database
    :migration-dir        "migrations/"
    :init-script          "init.sql"
    :init-in-transaction? false
-   :db                   (db-options)})
+   :db                   connectable})
 
-(defn migrate []
-  (with-locking db-lock
-    (migratus/init (migratus-config))
-    (migratus/migrate (migratus-config))))
-
-#_(migrate)
+(defn- migrate [db]
+  (with-locking (:lock db)
+    (let [config (migratus-config db)]
+      (migratus/init config)
+      (migratus/migrate config))))
 
 (mount/defstate db
-  :start (migrate))
+  :start (doto {:dbtype "sqlite"
+                :dbname (@config :db :path)
+                :lock (ReentrantLock.)}
+           migrate))
+
+#_(mount/start #'db)
 
 ;;;; DB interaction
 
@@ -42,8 +40,8 @@
     (if (<= tries 0)
       (raise 500 "Can't create a proper unused ID.")
       (let [id (new-id)
-            res (with-locking db-lock
-                  (jdbc/execute-one! (db-options) ["SELECT count(id) AS cnt FROM profile WHERE id = ?" id]))]
+            res (with-locking (:lock @db)
+                  (jdbc/execute-one! @db ["SELECT count(id) AS cnt FROM profile WHERE id = ?" id]))]
         (if (zero? (:cnt res))
           id
           (recur (dec tries)))))))
@@ -55,40 +53,42 @@
   (let [{:keys [id file_path profile_type sample_count owner upload_ts
                 edit_token is_public]} profile]
     (log/infof "Inserting profile %s from %s" id owner)
-    (with-locking db-lock
-      (sql-helpers/insert! (db-options) :profile
-                           {:id            id
-                            :file_path     file_path
-                            :profile_type  (name profile_type)
-                            :upload_ts     (str upload_ts)
-                            :sample_count  sample_count
-                            :is_public     is_public
-                            :edit_token    edit_token
-                            :owner         owner}))
+    (with-locking (:lock @db)
+      (jdbc/with-transaction [tx @db]
+        (sql-helpers/insert! tx :profile
+                             {:id            id
+                              :file_path     file_path
+                              :profile_type  (name profile_type)
+                              :upload_ts     (str upload_ts)
+                              :sample_count  sample_count
+                              :is_public     is_public
+                              :edit_token    edit_token
+                              :owner         owner})
+        (jdbc/execute-one! tx ["UPDATE stats SET val = val + 1 WHERE stat = 'total_uploaded'"])))
     profile))
 
-#_(jdbc/execute! (db-options) ["SELECT * FROM profile"])
+#_(jdbc/execute! @db ["SELECT * FROM profile"])
 
 (defn- unqualify-keys [m] (update-keys m (comp keyword name)))
 
 (defn list-profiles []
-  (with-locking db-lock
-    (->> (jdbc/execute! (db-options) ["SELECT id, file_path, profile_type, sample_count, owner, upload_ts, is_public FROM profile"])
+  (with-locking (:lock @db)
+    (->> (jdbc/execute! @db ["SELECT id, file_path, profile_type, sample_count, owner, upload_ts, is_public FROM profile"])
          (mapv #(-> (unqualify-keys %)
                     (assoc :edit_token nil)
                     (coerce Profile))))))
 
 (defn list-public-profiles [n]
-  (with-locking db-lock
-    (->> (jdbc/execute! (db-options) ["SELECT id, file_path, profile_type, sample_count, owner, upload_ts, is_public, edit_token FROM profile
+  (with-locking (:lock @db)
+    (->> (jdbc/execute! @db ["SELECT id, file_path, profile_type, sample_count, owner, upload_ts, is_public, edit_token FROM profile
 WHERE is_public = 1 ORDER BY upload_ts DESC LIMIT ?" n])
          (mapv #(-> (unqualify-keys %)
                     (coerce Profile))))))
 
 (defn get-profile [profile-id]
-  (with-locking db-lock
+  (with-locking (:lock @db)
     (let [q ["SELECT id, file_path, profile_type, sample_count, owner, upload_ts, edit_token, is_public FROM profile WHERE id = ?" profile-id]
-          row (some-> (jdbc/execute-one! (db-options) q)
+          row (some-> (jdbc/execute-one! @db q)
                       unqualify-keys
                       (coerce Profile))]
       (or row
@@ -97,16 +97,16 @@ WHERE is_public = 1 ORDER BY upload_ts DESC LIMIT ?" n])
             (raise 404 msg))))))
 
 (defn delete-profile [profile-id]
-  (with-locking db-lock
+  (with-locking (:lock @db)
     (let [q ["DELETE FROM profile WHERE id = ?" profile-id]
-          {::jdbc/keys [update-count]} (jdbc/execute-one! (db-options) q)]
+          {::jdbc/keys [update-count]} (jdbc/execute-one! @db q)]
       (when (zero? update-count)
         (raise 404 (format "Profile with ID '%s' not found." profile-id))))))
 
 (defn clear-db []
-  (with-locking db-lock
+  (with-locking (:lock @db)
     (.delete (clojure.java.io/file (@config :db :path)))
-    (migrate)))
+    (migrate @db)))
 
 (comment
   (clear-db)
--- test/flamebin/db_test.clj
+++ test/flamebin/db_test.clj
@@ -7,24 +7,25 @@
             [flamebin.dto :as dto]
             [flamebin.test-utils :refer :all]
             [flamebin.util :refer :all]
+            [mount.lite :as mount]
+            [flamebin.test-utils :refer :all]
             malli.generator
             [taoensso.timbre :as timbre]))
 
-(defmacro with-temp-db [& body]
-  `(let [f# (java.io.File/createTempFile "test-db" ".db")]
-     (try (with-config-redefs [[:db :path] f#]
-            (db/migrate)
-            ~@body)
-          (finally (.delete f#)))))
+(defmacro with-temp-db-and-state [& body]
+  `(try (with-temp-db
+          (doto #'db/db mount/stop mount/start)
+          ~@body)
+        (finally (doto #'db/db mount/stop mount/start))))
 
 (deftest init-test
-  (with-temp-db
+  (with-temp-db-and-state
     (is (= [] (db/list-profiles)))))
 
 (def ^:private inst1 (java.time.Instant/ofEpochSecond 1234567890))
 
 (deftest manual-test
-  (with-temp-db
+  (with-temp-db-and-state
     (db/insert-profile (dto/->Profile "QcXAqv" "some-path.dpf" "cpu" 12345
                                       nil "alhdslfglksjdfhg" true inst1))
     (is (= {:id "QcXAqv", :file_path "some-path.dpf", :profile_type :cpu,
@@ -47,7 +48,7 @@
    ;; Disable shrinking because it does little but slows down testing.
    [inserts (gen/vector (malli.generator/generator dto/Profile) 10 200)]
    (timbre/with-min-level :warn
-     (with-temp-db
+     (with-temp-db-and-state
        (run! db/insert-profile inserts)
        (let [fetched (db/list-profiles)
              no-pwd (fn [l] (mapv #(dissoc % :edit_token) l))]
